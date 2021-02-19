a4189c109e0c24cfcba180c2dade96be5eb212b4
Cesar Marquez <cmrodriguez17@esei.uvigo.es>
2021-02-19 20:22:58 +0100

    Setting Visual Studio project

--- src/HashCode2021.java
+++ src/HashCode2021.java
@@ -8,22 +8,32 @@ import java.util.ArrayList;
 public class HashCode2021 {
 
     /**
-     * @param args the command line arguments
+     * @param args archivo input
      */
     public static void main(String[] args) {
 
-        // Read file into input
-        ArrayList<String[]> input = Util.readFile(args[0]);
+        if (args.length != 2) {
 
-        // Pass input to problem
-        Problem problem = new Problem(input);
+            System.out.println("Uso: java HashCode2021 entrada.txt salida.txt");
 
-        // Solve problem and get result
-        String result = problem.solve();
+        } else {
 
-        // Write result to file
-        Util.writeFile(args[1], result);
+            System.out.println("Reading file... " + args[0]);
 
+            // Read file into input
+            ArrayList<String[]> input = Util.readFile(args[0]);
+
+            // Pass input to problem
+            Problem problem = new Problem(input);
+
+            // Solve problem and get result
+            String result = problem.solve();
+
+            // Write result to file
+            System.out.println("Writing file... " + args[1]);
+            Util.writeFile(args[1], result);
+
+        }
     }
 
 }
